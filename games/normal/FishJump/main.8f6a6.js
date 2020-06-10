d5b45607cc093e5764777a75446f7949fed51086
eatdreamcat <meow.std@outlook.com>
2020-06-10 15:13:01 +0800

    update fishjump

--- games/normal/FishJump/main.8f6a6.js
+++ games/normal/FishJump/main.8f6a6.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a3fa2.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.66c7b.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.0748a.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.8f6a6.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
