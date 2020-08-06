b65ccef80d7a84368cc3e685a3c3bd2ff9dc495b
eatdreamcat <meow.std@outlook.com>
2020-08-06 16:07:11 +0800

    commit by jenkins update FruitPunch..

--- games/normal/FruitPunch/main.b4d1f.js
+++ games/normal/FruitPunch/main.b4d1f.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.d9bec.js';
+        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.75c1b.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
