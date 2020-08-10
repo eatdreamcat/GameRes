2ab7056e3cae43546b35ed5dbc486fe51fd2a3f0
eatdreamcat <meow.std@outlook.com>
2020-08-10 18:29:39 +0800

    commit by jenkins update DominoCash..

--- games/normal/DominoCash/main.2056d.js
+++ games/normal/DominoCash/main.2056d.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.6826f.js';
+        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.98f60.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
