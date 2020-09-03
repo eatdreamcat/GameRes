4361ddce363f8e5dfd89f17766cca276153e2a1b
eatdreamcat <meow.std@outlook.com>
2020-09-03 10:46:09 +0800

    commit by jenkins update Bust-A-Move..

--- games/normal/Bust-A-Move-CheckN/main.c4398.js
+++ games/normal/Bust-A-Move-CheckN/main.c4398.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.1e7d1.js';
+        var bundledScript = settings.debug ? 'src/PROJECT_JS_DEBUG_PATH' : 'src/project.7d64d.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
