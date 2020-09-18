666f04b70ac19b163467230e52c2619abdcc0d20
eatdreamcat <meow.std@outlook.com>
2020-09-18 21:58:46 +0800

    commit by jenkins update SolitaireWin..

--- games/normal/SolitaireWin/main.b9194.js
+++ games/normal/SolitaireWin/main.b9194.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/project.3ab3e.js' : 'src/project.3ab3e.js';
+        var bundledScript = settings.debug ? 'src/project.35314.js' : 'src/project.35314.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
