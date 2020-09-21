9171cc4b79990839956e5f09f617677cf18680af
eatdreamcat <meow.std@outlook.com>
2020-09-21 12:04:16 +0800

    commit by jenkins update DaubCash..

--- games/normal/DaubCash/main.7dcfe.js
+++ games/normal/DaubCash/main.7dcfe.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/project.74bff.js' : 'src/project.74bff.js';
+        var bundledScript = settings.debug ? 'src/project.6e997.js' : 'src/project.6e997.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
