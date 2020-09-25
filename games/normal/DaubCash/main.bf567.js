b70b5414f282a76629d5cf75f3a608d2125e03d7
eatdreamcat <meow.std@outlook.com>
2020-09-25 16:34:29 +0800

    commit by jenkins update DaubCash..

--- games/normal/DaubCash/main.bf567.js
+++ games/normal/DaubCash/main.bf567.js
@@ -518,7 +518,7 @@ window.boot = function () {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/project.e66e9.js' : 'src/project.e66e9.js';
+        var bundledScript = settings.debug ? 'src/project.ea247.js' : 'src/project.ea247.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
