39c23505dc85ad5ec7637930c9ee3730d0b5c0cb
eatdreamcat <meow.std@outlook.com>
2021-09-24 18:19:21 +0800

    update by jenkins!

---
+++ games/normal/PoolCash/cocos3d-js/cc.physics-framework.js
@@ -0,0 +1,41 @@
+System.register(['./deprecated-e6a5ad41.js', './deprecated-d4c45d60.js', './index-3f109dd2.js', './SubContextView-6f635b0a.js', './component-event-handler-8a55e305.js', './index-334e7e0c.js', './terrain-asset-19c87035.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function () {}, function () {}, function () {}, function () {}, function () {}, function (module) {
+			var _setter = {};
+			_setter.BoxCollider = module.B;
+			_setter.BoxColliderComponent = module.B;
+			_setter.CapsuleCollider = module.d;
+			_setter.CapsuleColliderComponent = module.d;
+			_setter.Collider = module.C;
+			_setter.ColliderComponent = module.C;
+			_setter.ConeCollider = module.f;
+			_setter.ConstantForce = module.k;
+			_setter.Constraint = module.h;
+			_setter.CylinderCollider = module.e;
+			_setter.CylinderColliderComponent = module.e;
+			_setter.EAxisDirection = module.a;
+			_setter.ERigidBodyType = module.E;
+			_setter.HingeConstraint = module.H;
+			_setter.MeshCollider = module.M;
+			_setter.MeshColliderComponent = module.M;
+			_setter.PhysicMaterial = module.j;
+			_setter.PhysicsRayResult = module.b;
+			_setter.PhysicsSystem = module.P;
+			_setter.PlaneCollider = module.g;
+			_setter.PointToPointConstraint = module.i;
+			_setter.RigidBody = module.R;
+			_setter.RigidBodyComponent = module.R;
+			_setter.SimplexCollider = module.S;
+			_setter.SphereCollider = module.c;
+			_setter.SphereColliderComponent = module.c;
+			_setter.TerrainCollider = module.T;
+			exports(_setter);
+		}, function () {}],
+		execute: function () {
+
+
+
+		}
+	};
+});
---
+++ games/normal/PoolCash/cocos3d-js/cc.wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./ammo-instantiated-f26a4f0a.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.w);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
