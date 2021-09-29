594dd4c9dbf694de2ae8c3bda6b68b8d1331826a
eatdreamcat <meow.std@outlook.com>
2021-09-29 11:15:38 +0800

    update by jenkins!

---
+++ games/normal/PoolCash/cocos3d-js/cc.wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./ammo-instantiated-4b72453e.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.w);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
